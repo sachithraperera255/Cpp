1c5bbf86259dba4e54ca97b5fec13ee8976ad345
sachithraperera255 <sachithraperera255@gmail.com>
2026-02-23 23:16:48 -0500

    What is padding in structures

--- structures.cpp
+++ structures.cpp
@@ -26,6 +26,19 @@ struct Person
 };
 
 
+/*
+    - Just like in C language size of the structure is determined by sum of the its data members
+    - Compiler will add necessary padding to align memory.
+*/
+struct sizeofstruct
+{
+    char character;
+    int a;
+    int b;
+};
+
+
+
 int main ()
 {
     Person p1;
@@ -33,8 +46,19 @@ int main ()
     p1.height = 5.6;
     // std::cout<<"age: "<<p1.age<<std::endl;
     // std::cout<<"height: "<<p1.height<<std::endl;
-
     p1.printinfo();
+
+    std::cout<<"Size of the structure "<<sizeof(sizeofstruct)<<std::endl;
+    /*
+        - The output of the above line is 12 (12-bytes) because
+        char character = 1 byte
+        int a          = 4 byte
+        int b          = 4 byte
+        padding        = 3 byte
+
+        The processor read as in word which equal to 4-bytes. When char uses 1-byte in the word,
+        rest of the 3-bytes in the word will be padded to align the memory.
+    */
     
     return 0;
 }
